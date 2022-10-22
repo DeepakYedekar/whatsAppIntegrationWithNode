26c0bc8932810309a833f90dd76d068ced23a1d0
Deepak <deepakyedekar28@gmail.com>
2022-10-22 16:54:12 +0530

    changed

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ app.get('/api', (req,res) => {
     let mode = req.query['hub.mode'];
     let challenge = req.query['hub.challenge'];
     let token = req.query['hub.verify_token'];
-    const mytoken = "";
     if (mode && token) {
         if (mode === 'subscribe' && token == process.env.MYTOKEN) {
             res.status(200).send(challenge);
@@ -18,38 +17,38 @@ app.get('/api', (req,res) => {
     }
 })
 
-app.post('/api', (req, res) => {
-    let body_param = req.body;
-    console.log('its working')
-    // console.log(JSON.stringify(body_param, null, 2));
+// app.post('/api', (req, res) => {
+//     let body_param = req.body;
+//     console.log('its working')
+//     console.log(JSON.stringify(body_param, null, 2));
 
-    // if (body_param.object) {
-    //     if (body_param.entry && body_param.entry[0].changes && 
-    //         body_param.entry[0].changes[0].values.messages &&
-    //         body_param.entry[0].changes[0].values.messages[0]) {
-    //         let phone_no_id = body.entry.changes[0].values.metadata.phone_number_id;
-    //         let from = body_param.entry[0].changes[0].values.messages[0].from;
-    //         let msg_body = body_param.entry[0].changes[0].values.messages[0].text.body;
-    //         axios({
-    //             method: 'POST',
-    //             url: "https://graph.facebook.com/v14.0/" + phone_no_id + "/messages?access_token=" + token,
-    //             data: {
-    //                 messaging_product: 'whatsapp',
-    //                 to: from,
-    //                 text: {
-    //                     body:"Hi...i am Deepak"
-    //                 }
-    //             },
-    //             headers: {
-    //                 "Context-Type":"application/json"
-    //             }
-    //         })
-    //         res.sendStatus(200);
-    //     } else {
-    //         res.sendStatus(404);
-    //         }
-    // }
-})
+//     if (body_param.object) {
+//         if (body_param.entry && body_param.entry[0].changes && 
+//             body_param.entry[0].changes[0].values.messages &&
+//             body_param.entry[0].changes[0].values.messages[0]) {
+//             let phone_no_id = body.entry.changes[0].values.metadata.phone_number_id;
+//             let from = body_param.entry[0].changes[0].values.messages[0].from;
+//             let msg_body = body_param.entry[0].changes[0].values.messages[0].text.body;
+//             axios({
+//                 method: 'POST',
+//                 url: "https://graph.facebook.com/v14.0/" + phone_no_id + "/messages?access_token=" + token,
+//                 data: {
+//                     messaging_product: 'whatsapp',
+//                     to: from,
+//                     text: {
+//                         body:"Hi...i am Deepak"
+//                     }
+//                 },
+//                 headers: {
+//                     "Context-Type":"application/json"
+//                 }
+//             })
+//             res.sendStatus(200);
+//         } else {
+//             res.sendStatus(404);
+//             }
+//     }
+// })
 
 
 app.get('/', (req, res) => {
