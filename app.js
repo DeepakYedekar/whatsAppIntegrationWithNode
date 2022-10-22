f14b6675ca2166a85e8a90bbc3ea21c4f3c46202
Deepak <deepakyedekar28@gmail.com>
2022-10-22 18:19:17 +0530

    changes

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ const axios = require('axios');
 const app = express();
 require('dotenv').config();
 
+const token=process.env.TOKEN;
+const mytoken=process.env.MYTOKEN;
 app.get('/api', (req,res) => {
     let mode = req.query['hub.mode'];
     let challenge = req.query['hub.challenge'];
     let token = req.query['hub.verify_token'];
     if (mode && token) {
-        if (mode === 'subscribe' && token === process.env.MYTOKEN) {
+        if (mode === 'subscribe' && token === mytoken) {
             res.status(200).send(challenge);
         } else {
             res.status(403);
@@ -17,34 +19,45 @@ app.get('/api', (req,res) => {
 })
 
 app.post('/api', (req, res) => {
-    let body_param = req.body;
-    if (body_param.object) {
-        if (body_param.entry && body_param.entry[0].changes && 
-            body_param.entry[0].changes[0].values.messages &&
-            body_param.entry[0].changes[0].values.messages[0]) {
-            
-            let phone_no_id = body_param.entry[0].changes[0].value.metadata.phone_number_id;
-            let from = body_param.entry[0].changes[0].values.messages[0].from;
-            let msg_body = body_param.entry[0].changes[0].values.messages[0].text.body;
-            axios({
-                method: 'POST',
-                url: "https://graph.facebook.com/v14.0/" + phone_no_id + "/messages?access_token=" + process.env.TOKEN,
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                data: {
-                     "messaging_product": "whatsapp",
-                     "to": from,
-                    "text": {
-                         "body":"hi............"
-                     }
-                }
-            }).then((response) => {
-                res.status(200).send(response.data);
-            })
-        } else {
-            res.status(400).send('error');
+  let body_param=req.body;
+    console.log(JSON.stringify(body_param,null,2));
+
+    if(body_param.object){
+        console.log("inside body param");
+        if(body_param.entry && 
+            body_param.entry[0].changes && 
+            body_param.entry[0].changes[0].value.messages && 
+            body_param.entry[0].changes[0].value.messages[0]  
+            ){
+               let phon_no_id=body_param.entry[0].changes[0].value.metadata.phone_number_id;
+               let from = body_param.entry[0].changes[0].value.messages[0].from; 
+               let msg_body = body_param.entry[0].changes[0].value.messages[0].text.body;
+
+               console.log("phone number "+phon_no_id);
+               console.log("from "+from);
+               console.log("boady param "+msg_body);
+
+               axios({
+                   method:"POST",
+                   url:"https://graph.facebook.com/v13.0/"+phon_no_id+"/messages?access_token="+token,
+                   data:{
+                       messaging_product:"whatsapp",
+                       to:from,
+                       text:{
+                           body:"Hi.. I'm Prasath, your message is "+msg_body
+                       }
+                   },
+                   headers:{
+                       "Content-Type":"application/json"
+                   }
+
+               });
+
+               res.sendStatus(200);
+            }else{
+                res.sendStatus(404);
             }
+
     }
 })
 
