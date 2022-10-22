407e681e9328201e7c8d7f521b2160b43d807883
Deepak <deepakyedekar28@gmail.com>
2022-10-22 18:30:11 +0530

    modified

--- app.js
+++ app.js
@@ -39,9 +39,10 @@ app.post('/api', (req, res) => {
 
                axios({
                    method:"POST",
-                   url:"https://graph.facebook.com/v13.0/"+phon_no_id+"/messages?access_token="+token,
+                   url:"https://graph.facebook.com/v14.0/"+phone_number_id+"/messages?access_token="+token,
                    data:{
-                       messaging_product:"whatsapp",
+                       messaging_product: "whatsapp",
+                       recipient_type: "individual",
                        to:from,
                        text:{
                            body:"Hi.. I'm Prasath, your message is "+msg_body
